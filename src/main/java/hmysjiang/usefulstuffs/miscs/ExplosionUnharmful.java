5375c0832070d8b4a91f24849d8038a69a3dfa94
hm-ysjiang <30855524+hm-ysjiang@users.noreply.github.com>
2018-05-07 23:13:05 +0800

    Add files via upload

--- src/main/java/hmysjiang/usefulstuffs/miscs/ExplosionUnharmful.java
+++ src/main/java/hmysjiang/usefulstuffs/miscs/ExplosionUnharmful.java
@@ -50,47 +50,47 @@ public class ExplosionUnharmful extends Explosion {
 	@Override
 	public void doExplosionA() {
 		Set<BlockPos> set = Sets.<BlockPos>newHashSet();
-        int i = 16;
+		int i = 16;
 
-        for (int j = 0; j < 16; ++j) {
-        	for (int k = 0; k < 16; ++k) {
-            	for (int l = 0; l < 16; ++l) {
-                	if (j == 0 || j == 15 || k == 0 || k == 15 || l == 0 || l == 15) {
-                    	double d0 = (double)((float)j / 15.0F * 2.0F - 1.0F);
-                        double d1 = (double)((float)k / 15.0F * 2.0F - 1.0F);
-                        double d2 = (double)((float)l / 15.0F * 2.0F - 1.0F);
-                        double d3 = Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
-                        d0 = d0 / d3;
-                        d1 = d1 / d3;
-                        d2 = d2 / d3;
-                        float f = this.explosionSize * (0.7F + this.world.rand.nextFloat() * 0.6F);
-                        double d4 = this.explosionX;
-                        double d6 = this.explosionY;
-                        double d8 = this.explosionZ;
+		for (int j = 0; j < 16; ++j) {
+			for (int k = 0; k < 16; ++k) {
+				for (int l = 0; l < 16; ++l) {
+					if (j == 0 || j == 15 || k == 0 || k == 15 || l == 0 || l == 15) {
+						double d0 = (double)((float)j / 15.0F * 2.0F - 1.0F);
+						double d1 = (double)((float)k / 15.0F * 2.0F - 1.0F);
+						double d2 = (double)((float)l / 15.0F * 2.0F - 1.0F);
+						double d3 = Math.sqrt(d0 * d0 + d1 * d1 + d2 * d2);
+						d0 = d0 / d3;
+						d1 = d1 / d3;
+						d2 = d2 / d3;
+						float f = this.explosionSize * (0.7F + this.world.rand.nextFloat() * 0.6F);
+						double d4 = this.explosionX;
+						double d6 = this.explosionY;
+						double d8 = this.explosionZ;
 
-                        for (float f1 = 0.3F; f > 0.0F; f -= 0.22500001F) {
-                        	BlockPos blockpos = new BlockPos(d4, d6, d8);
-                            IBlockState iblockstate = this.world.getBlockState(blockpos);
+						for (float f1 = 0.3F; f > 0.0F; f -= 0.22500001F) {
+							BlockPos blockpos = new BlockPos(d4, d6, d8);
+							IBlockState iblockstate = this.world.getBlockState(blockpos);
 
-                            if (iblockstate.getMaterial() != Material.AIR)
-                            {
-                                float f2 = iblockstate.getBlock().getExplosionResistance(world, blockpos, (Entity)null, this);
-                                f -= (f2 + 0.3F) * 0.3F;
-                            }
+							if (iblockstate.getMaterial() != Material.AIR)
+							{
+								float f2 = iblockstate.getBlock().getExplosionResistance(world, blockpos, (Entity)null, this);
+								f -= (f2 + 0.3F) * 0.3F;
+							}
 
-                            if (f > 0.0F) 
-                            	set.add(blockpos);
+							if (f > 0.0F) 
+								set.add(blockpos);
 
-                            d4 += d0 * 0.30000001192092896D;
-                            d6 += d1 * 0.30000001192092896D;
-                            d8 += d2 * 0.30000001192092896D;
-                        }
-                    }
-                }
-            }
-        }
+							d4 += d0 * 0.30000001192092896D;
+							d6 += d1 * 0.30000001192092896D;
+							d8 += d2 * 0.30000001192092896D;
+						}
+					}
+				}
+			}
+		}
 
-        this.affectedBlockPositions.addAll(set);
+		this.affectedBlockPositions.addAll(set);
 	}
 	
 	@Override
